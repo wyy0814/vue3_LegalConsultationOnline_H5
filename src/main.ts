44cf09515d8efa4469dc7ab1e6c3905630ec12df
吴延勇 <1598127236@qq.com>
2023-08-08 22:49:42 +0800

    删除原始代码，抽离pinia为单独模块

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import pinia from './stores'
 
+// 创建vue根实例
 const app = createApp(App)
 
-app.use(createPinia())
+// 注册pinia和router
 app.use(router)
+app.use(pinia)
 
+// 挂载渲染App根组件
 app.mount('#app')
---
+++ src/stores/index.ts
@@ -0,0 +1,10 @@
+/* 
+  创建pinia实例并导出
+*/
+import { createPinia } from 'pinia'
+
+// 创建pinia实例
+const pinia = createPinia()
+
+// 导出pinia实例
+export default pinia
