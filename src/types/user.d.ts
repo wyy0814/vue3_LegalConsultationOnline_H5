5af219c4b43d67440dfb598352bea56653f6b110
吴延勇 <1598127236@qq.com>
2023-08-08 22:57:59 +0800

    增加登录接口返回的数据的数据类型

---
+++ src/types/user.d.ts
@@ -0,0 +1,17 @@
+/* 
+    用户相关ts类型
+*/
+
+// 登录接口返回数据类型
+export type User = {
+  /** token令牌 */
+  token: string
+  /** 用户ID */
+  id: string
+  /** 用户名称 */
+  account: string
+  /** 手机号 */
+  mobile: string
+  /** 头像 */
+  avatar: string
+}
