9741aa19f8e74212b8dae4ff0bc0d37021f91916
吴延勇 <1598127236@qq.com>
2023-08-10 21:48:55 +0800

    完成了登录页面的样式、结构

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,13 +6,112 @@ const clickRight = () => {
 
 <template>
   <div class="login-page">
+    <!-- 头部导航栏 -->
     <law-nav-bar
       title="登录"
       right="注册"
       @click-right="clickRight"
     ></law-nav-bar>
-    login
+    <!-- 头部 -->
+    <div class="login-head">
+      <h3>密码登录</h3>
+      <a href="javascript:;">
+        <span>短信验证码登录</span>
+        <van-icon name="arrow"></van-icon>
+      </a>
+    </div>
+    <!-- 表单 -->
+    <van-form autocomplete="off">
+      <van-field placeholder="请输入手机号" type="tel"></van-field>
+      <van-field placeholder="请输入密码" type="password"></van-field>
+      <div class="cp-cell">
+        <van-checkbox>
+          <span>我已同意</span>
+          <a href="javascript:;">用户协议</a>
+          <span>及</span>
+          <a href="javascript:;">隐私条款</a>
+        </van-checkbox>
+      </div>
+      <div class="cp-cell">
+        <van-button block round type="primary">登 录</van-button>
+      </div>
+      <div class="cp-cell forget">
+        <a href="javascript:;">忘记密码？</a>
+      </div>
+    </van-form>
+    <!-- 底部 -->
+    <div class="login-other">
+      <van-divider>第三方登录</van-divider>
+      <div class="icon">
+        <img src="@/assets/qq.svg" alt="" />
+      </div>
+    </div>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.login {
+  &-page {
+    padding-top: 46px;
+  }
+  &-head {
+    // background: yellow;
+    display: flex;
+    // padding: 30px 30px 50px;
+    justify-content: space-between;
+    align-items: center;
+    padding: 30px 30px 10px 30px;
+    line-height: 1;
+    h3 {
+      // background: green;
+      font-weight: normal;
+      font-size: 24px;
+    }
+    a {
+      font-size: 15px;
+      // background: pink;
+    }
+  }
+  &-other {
+    margin-top: 50px;
+    padding: 0 30px;
+    .icon {
+      display: flex;
+      justify-content: center;
+      img {
+        width: 36px;
+        height: 36px;
+        padding: 4px;
+      }
+    }
+  }
+}
+.van-form {
+  // background: pink;
+  padding: 0 14px;
+  .forget {
+    font-size: 15px;
+    // color: balck;
+  }
+  .cp-cell {
+    height: 52px;
+    line-height: 24px;
+    padding: 14px 16px;
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    .van-checkbox {
+      a {
+        color: pink;
+        padding: 0 5px;
+      }
+    }
+  }
+  .btn-send {
+    color: pink;
+    &.active {
+      color: rgba(22, 194, 163, 0.5);
+    }
+  }
+}
+</style>
